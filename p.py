9f178d2c1ec51b8f727f9777f5b85b6c281c8a03
NoveltyOFD <vnovelty.ofd@gmail.com>
2017-02-21 20:03:22 +0700

    asd

---
+++ p.py
@@ -0,0 +1,27 @@
+"""scoreA = [4,2,9,7,2,3]
+scoreB = [3,2,1,5,6,3] 
+A=0
+B=0
+for i in range(len(scoreA)) :
+	if  scoreA[i] > scoreB[i]:
+		A = A + 1
+	elif scoreA[i] < scoreB[i]:
+		B = B + 1
+
+
+if (A > B):
+	print ("Alice menang dengan skor", A)
+elif (A < B):
+	print("Bob menang dengan skor", B)
+else:
+	print ("Seri cuy dengan skor", A)
+"""
+
+A = [int(x) for x in input().split(' ')]
+
+for x in range(5,1,-3)
+	print x
+
+
+print(A)
+
